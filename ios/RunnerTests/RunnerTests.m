e5809fa3e72cb7df6abfb1b9985f8ce77c0cc0e9
LuongTH <luongth@mti-tech.vn>
2024-08-23 11:45:58 +0700

    add ios config

---
+++ ios/RunnerTests/RunnerTests.m
@@ -0,0 +1,11 @@
+//
+//  RunnerTests.m
+//  RunnerTests
+//
+//  Created by Tran Huu Luong(LuongTH) on 23/08/2024.
+//
+
+@import XCTest;
+@import integration_test;
+
+INTEGRATION_TEST_IOS_RUNNER(RunnerTests)
--- tools/run_firebase_test.sh
+++ tools/run_firebase_test.sh
@@ -17,6 +17,28 @@ gcloud firebase test android run \
   --app build/app/outputs/apk/debug/app-debug.apk \
   --test build/app/outputs/apk/androidTest/debug/app-debug-androidTest.apk \
 
+# Run for iOS
+output="../build/ios_integ"
+product="build/ios_integ/Build/Products"
+dev_target="17.2"
+PRODUCT_NAME="Release-iphoneos"
+TEST_RUNNER="iphoneos$dev_target-arm64.xctestrun"
+
+flutter clean
+
+flutter build ios integration_test/app_test.dart --simulator
+
+pushd ios
+xcodebuild -workspace Runner.xcworkspace -config Release -derivedDataPath $output -sdk iphoneos build-for-testing
+popd
+
+pushd $product
+zip -r "ios_tests.zip" $PRODUCT_NAME $TEST_RUNNER
+popd
+
+gcloud firebase test ios run \
+  --test "build/ios_integ/Build/Products/ios_tests.zip" \
+
 # Check exit code from Firebase Test Lab
 EXIT_CODE=$?
 if [ $EXIT_CODE -eq 0 ]; then
